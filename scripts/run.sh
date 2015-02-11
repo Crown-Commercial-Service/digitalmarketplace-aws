b523118d501e13ef901e24c7a552eb59ded5dfee
Alexey Bezhan <al@fueledbylemons.com>
2015-02-11 17:14:13 +0000

    Add environment-specific vars files

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 [ "$#" -eq 1 ] || { echo "AWS key name argument required"; exit 1; }
 
-export AWS_KEY_NAME=$1
+AWS_KEY_NAME=$1
 export ANSIBLE_CONFIG=playbooks/ansible.cfg
 
-ansible-playbook playbooks/playbook.yml -i playbooks/hosts --private-key=~/.ssh/$AWS_KEY_NAME
+ansible-playbook playbooks/playbook.yml -i playbooks/hosts --private-key=~/.ssh/$AWS_KEY_NAME -e "environment=$DEPLOY_ENV keyname=$AWS_KEY_NAME"
