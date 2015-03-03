af2d0b7cf21b21d5f707910e46ce984736a903f6
Alexey Bezhan <al@fueledbylemons.com>
2015-03-03 11:25:44 +0000

    Rewrite run/scripts with common options decorator
    * stage and environment are now positional arguments
    * Options come after the command name
    * New options to pass additional variables and variable files
    * Immutable context to isolate chained command arguments

--- scripts/run.py
+++ scripts/run.py
@@ -4,6 +4,8 @@
 import os
 import json
 import subprocess
+from collections import namedtuple
+from functools import wraps
 
 import click
 
@@ -13,6 +15,21 @@ STAGES = [
 ]
 
 
+_Params = namedtuple('_Params', ['variables', 'options', 'tags'])
+
+
+class PlaybookParams(_Params):
+    def add(self, variables=None, options=None, tags=None):
+        params = self._replace(
+            variables=self.variables.copy(),
+            options=self.options + list(options or []),
+            tags=self.tags + list(tags or [])
+        )
+        params.variables.update(variables or {})
+
+        return params
+
+
 def run_cmd(args, env=None):
     cmd_env = os.environ.copy()
     cmd_env.update(env)
@@ -20,8 +37,7 @@ def run_cmd(args, env=None):
     cmd.communicate()
 
 
-def run_playbook(playbook, hosts, options, variables,
-                 tags=None, basedir='playbooks/'):
+def run_playbook(playbook, hosts, params, basedir='playbooks/'):
     args = [
         'ansible-playbook',
         os.path.join(basedir, playbook + '.yml'),
@@ -29,15 +45,15 @@ def run_playbook(playbook, hosts, options, variables,
         os.path.join(basedir, hosts),
     ]
 
-    args.extend(options)
     args.extend([
         '-e',
-        json.dumps(variables)
+        json.dumps(params.variables)
     ])
+    args.extend(params.options)
 
-    tags_option = '--tags={}'.format(','.join(tags))
+    tags_option = '--tags={}'.format(','.join(params.tags))
 
-    if tags:
+    if params.tags:
         args.append(tags_option)
 
     click.echo(subprocess.list2cmdline(args))
@@ -50,55 +66,71 @@ def run_playbook(playbook, hosts, options, variables,
 @click.group(chain=True)
 @click.argument('stage', nargs=1, type=click.Choice(STAGES))
 @click.argument('environment', nargs=1)
-@click.option('--tag', '-t', multiple=True)
 @click.pass_context
-def main(ctx, stage, environment, tag):
-
-    variables = {
+def main(ctx, stage, environment):
+    ctx.obj = ctx.obj.add(variables={
         'stage_name': stage,
         'environment_name': environment,
-    }
-
-    ctx.obj['options'] = []
-    ctx.obj['variables'] = variables
-    ctx.obj['tags'] = tag
+    })
 
 
-@main.command()
-@click.pass_context
+def deploy_command(f):
+    """Common options for deployment commands."""
+    @click.option('--tag', '-t', multiple=True,
+                  help="Only run tasks with given tag")
+    @click.option('--extra', '-e', multiple=True,
+                  help="Set an extra variable (key=value or YAML/JSON string)")
+    @click.option('--vars-file', '-f', multiple=True,
+                  type=click.Path(exists=True),
+                  help="Load YAML or JSON extra variable file")
+    @click.pass_context
+    @wraps(f)
+    def wrapped(ctx, tag, extra, vars_file, *args, **kwargs):
+        file_options = sum([['-e', '@{}'.format(v)] for v in vars_file], [])
+        extra_vars_options = sum([['-e', e] for e in extra], [])
+        ctx.obj = ctx.obj.add(
+            tags=tag,
+            options=file_options + extra_vars_options
+        )
+        return f(ctx, *args, **kwargs)
+
+    return wrapped
+
+
+@main.command('setup')
+@deploy_command
 @click.option('--dev-access', '-d', is_flag=True, default=False,
               help="Open service ports for access from user_cidr_ip")
-def setup(ctx, dev_access):
+def setup_cmd(ctx, dev_access):
     """Create AWS environment and launch instances"""
-    kwargs = ctx.obj.copy()
-    kwargs['variables'] = ctx.obj['variables'].copy()
-    kwargs['variables'].update({
+
+    params = ctx.obj.add(variables={
         'dev_access_state': 'present' if dev_access else 'absent',
     })
 
-    run_playbook('setup', 'hosts', **kwargs)
+    run_playbook('setup', 'hosts', params)
 
 
-@main.command()
-@click.pass_context
-def provision(ctx):
+@main.command('provision')
+@deploy_command
+def provision_cmd(ctx):
     """Provision EC2 instances"""
-    run_playbook('provision', 'ec2.py', **ctx.obj)
+    run_playbook('provision', 'ec2.py', ctx.obj)
 
 
-@main.command()
-@click.pass_context
+@main.command('teardown')
+@deploy_command
 @click.option('--with-base/--skip-base', default=False,
               help="Remove shared base resources")
-def teardown(ctx, with_base):
+def teardown_cmd(ctx, with_base):
     """Destroy AWS environment and terminate running instances."""
 
-    kwargs = ctx.obj.copy()
-    if not with_base:
-        kwargs['options'] = ctx.obj['options'] + ['--skip-tags=base']
+    params = ctx.obj.add(
+        options=['--skip-tags=base'] if (not with_base) else None
+    )
 
-    run_playbook('teardown', 'hosts', **kwargs)
+    run_playbook('teardown', 'hosts', params)
 
 
 if __name__ == '__main__':
-    main(obj={}, auto_envvar_prefix='AWS')
+    main(obj=PlaybookParams({}, [], []), auto_envvar_prefix='AWS')
