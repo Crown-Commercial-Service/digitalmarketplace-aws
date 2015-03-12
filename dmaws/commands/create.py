a4d3936da1fe899acfcead7e7bdf2e05e2cc8dc8
Alexey Bezhan <al@fueledbylemons.com>
2015-03-12 13:47:47 +0000

    Remove unused create -d flag

--- dmaws/commands/create.py
+++ dmaws/commands/create.py
@@ -1,14 +1,10 @@
-import click
-
 from ..stacks import StackPlan
 from ..cli import main, cli_command
 
 
 @main.command('create')
 @cli_command
-@click.option('--dev-access', '-d', is_flag=True, default=False,
-              help="Open service ports for access from user_cidr_ip")
-def create_cmd(ctx, dev_access):
+def create_cmd(ctx):
     """Create AWS environment and launch instances"""
 
     plan = StackPlan.from_ctx(ctx)
