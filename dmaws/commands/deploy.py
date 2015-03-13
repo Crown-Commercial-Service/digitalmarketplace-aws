f25023f5a9c49c19ca8715f53ede0b01497b6ceb
Alexey Bezhan <al@fueledbylemons.com>
2015-03-13 10:25:36 +0000

    Add deploy command
    Adds dmaws.deploy that's responsible for creating a new
    application version, uploading the release and starting the
    Elastic Beanstalk deploy process.
    
    The `deploy` command takes one argument: the path to the project
    repository. The application name is taken from the repository origin,
    which should look like digitalmarketplace-<app>.

---
+++ dmaws/commands/deploy.py
@@ -0,0 +1,27 @@
+import click
+
+from ..cli import cli_command
+from ..stacks import StackPlan
+from ..build import get_application_name
+from ..deploy import Deploy
+
+
+@click.argument('repository_path', nargs=1, type=click.Path(exists=True))
+@cli_command('deploy', max_apps=0)
+def deploy_cmd(ctx, repository_path):
+    """Deploy a new application version to the Elastic Beanstalk environment."""
+
+    app = get_application_name(repository_path)
+    ctx.add_apps(app)
+    stack_info = StackPlan.from_ctx(ctx).info()[ctx.apps[0]]
+
+    deploy = Deploy(
+        stack_info.parameters['ApplicationName'],
+        stack_info.parameters['EnvironmentName'],
+        repository_path,
+        region=ctx.variables['aws_region'],
+        logger=ctx.log
+    )
+
+    version = deploy.create_version(app, with_sha=True)
+    deploy.deploy(version)
---
+++ dmaws/deploy.py
@@ -0,0 +1,90 @@
+import os
+
+from . import build
+
+import boto.beanstalk
+import boto.s3
+import boto.exception
+
+
+class Deploy(object):
+    def __init__(self, eb_application, eb_environment, repo_path, region, logger=None):
+        self.log = logger
+        self.beanstalk = BeanstalkClient(region)
+        self.s3 = S3Client(region)
+
+        self.repo_path = repo_path
+        self.eb_application = eb_application
+        self.eb_environment = eb_environment
+
+    def create_version(self, version_label, with_sha=False, description=''):
+        ref, sha, package_path = build.create_archive(self.repo_path)
+        self.log('Created a git archive at %s', package_path)
+
+        if with_sha:
+            version_label = '{}-{}-{}'.format(version_label, ref, sha[:7])
+        package_name = '{}/{}.zip'.format(self.eb_application, version_label)
+
+        bucket = self.beanstalk.get_storage_location()
+
+        self.log('Uploading %s to %s/%s', package_path, bucket, package_name)
+        _, s3_key = self.s3.upload_package(bucket, package_name, package_path)
+
+        self.log('Creating a new %s version: %s', self.eb_application, version_label)
+        self.beanstalk.create_application_version(
+            self.eb_application,
+            version_label,
+            bucket,
+            s3_key,
+            description
+        )
+
+        self.log("Removing package file %s", package_path)
+        os.remove(package_path)
+
+        return version_label
+
+    def deploy(self, version_label):
+        self.log("Deploying version %s to %s", version_label, self.eb_environment)
+        self.beanstalk.update_environment(self.eb_environment, version_label)
+
+
+class S3Client(object):
+    def __init__(self, region, **kwargs):
+        self.conn = boto.s3.connect_to_region(region)
+
+    def upload_package(self, bucket_name, package_name, package_file):
+        bucket = self.conn.get_bucket(bucket_name)
+        key = bucket.new_key(package_name)
+        key.set_contents_from_filename(package_file)
+
+        return bucket, key.key
+
+
+class BeanstalkClient(object):
+    def __init__(self, region):
+        self.conn = boto.beanstalk.connect_to_region(region)
+
+    def get_storage_location(self):
+        response = self.conn.create_storage_location()['CreateStorageLocationResponse']
+        return response['CreateStorageLocationResult']['S3Bucket']
+
+    def update_environment(self, environment_name, version_label):
+        self.conn.update_environment(
+            environment_name=environment_name,
+            version_label=version_label
+        )
+
+    def create_application_version(self, application_name, version_label,
+                                   s3_bucket, s3_key, description):
+        try:
+            self.conn.create_application_version(
+                application_name,
+                version_label,
+                s3_bucket=s3_bucket,
+                s3_key=s3_key,
+                description=description
+            )
+        except boto.exception.BotoServerError as e:
+            if '{} already exists'.format(version_label) not in e.message:
+                raise
