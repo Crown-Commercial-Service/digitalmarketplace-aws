aca0477097bf62e91f2cb54a7f0d94e29e384341
Alexey Bezhan <al@fueledbylemons.com>
2015-04-02 23:49:34 +0100

    Run pep8 after running tests in tun_tests.sh
    Tests are more important and give more meaningful output, so
    should be run first.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -20,8 +20,8 @@ function display_result {
   fi
 }
 
-pep8 .
-display_result $? 1 "Code style check"
-
 py.test -v
 display_result $? 2 "Unit tests"
+
+pep8 .
+display_result $? 1 "Code style check"
