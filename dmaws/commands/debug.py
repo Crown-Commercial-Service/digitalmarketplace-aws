6dd4a8df63ca9c31e143b6d8d8e16962a4c8060f
Alexey Bezhan <al@fueledbylemons.com>
2015-04-14 13:23:54 +0100

    Add debug-value and debug-template CLI commands
    debug-value allows looking up the variable/stack values using the
    dotted notation, e.g. stacks.api.parameters.Domain
    
    debug-template outputs the CloudFormation template body after the
    Jinja processing.

---
+++ dmaws/commands/debug.py
@@ -0,0 +1,26 @@
+import click
+
+from ..cli import cli_command
+from ..stacks import StackPlan
+
+
+@cli_command('debug-value', max_apps=0)
+@click.argument('values', nargs=-1)
+def debug_value_cmd(ctx, values):
+    """Get values of the given dotted variables."""
+
+    plan = StackPlan.from_ctx(ctx, apps=['all'], logger=None)
+    plan.info(with_aws=False)
+
+    for value in values:
+        ctx.log("%s: %s", value, plan.get_value(value))
+
+
+@cli_command('debug-template', max_apps=1)
+def debug_template_cmd(ctx):
+    """Print the app CloudFormation template after Jinja processing."""
+    plan = StackPlan.from_ctx(ctx, logger=None)
+
+    for name, stack in plan.stacks(with_dependencies=False):
+        built_stack = plan.build_stack(stack)
+        ctx.log(built_stack.template_body)
