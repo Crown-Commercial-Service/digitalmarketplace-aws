a6b41393fec32371bd9e52ca2a2dc86489aff316
Alexey Bezhan <al@fueledbylemons.com>
2016-09-18 16:31:26 +0100

    Add a helper command to export Jenkins deployments to GitHub
    Requires a JSON export of Jenkins deployments and creates a
    Github Deployment for each build.

--- dmaws/commands/deploy.py
+++ dmaws/commands/deploy.py
@@ -1,10 +1,13 @@
 import sys
+import json
+from datetime import datetime
 
 import click
 
-from ..cli import cli_command
+from ..cli import cli_command, base
 from ..stacks import StackPlan
 from ..build import get_application_name
+from ..github import publish_deployment
 
 
 @click.argument('repository_path', nargs=1, type=click.Path(exists=True))
@@ -24,3 +27,26 @@ def deploy_cmd(ctx, repository_path):
         sys.exit(1)
 
     ctx.log("URL: http://%s/", url)
+
+
+@click.argument('deployments_json', nargs=1, type=click.File())
+@click.option('--github-token', help="Github API token", default=None)
+@base.command('publish-deployments')
+def publish_releases(deployments_json, github_token):
+    """Publishes Jenkins deployments JSON dump to Github Deployments API"""
+
+    releases = json.load(deployments_json)
+    return all(
+        publish_deployment(
+            token=github_token,
+            repo=release['repo'],
+            ref=release['release'],
+            environment=release['stage'],
+            build=release['build'],
+            created_at=datetime.utcfromtimestamp(release['timestamp'] / 1000.0),
+            ci_url=release['build_url'],
+            status=release['status'],
+            logger=click.echo
+        )
+        for release in releases
+    )
