4d86b1ce1782b76de22b01f5836aca42714f2a32
Alexey Bezhan <al@fueledbylemons.com>
2016-09-20 10:53:51 +0100

    Test that Github API is called with the correct payload

--- tests/test_github.py
+++ tests/test_github.py
@@ -28,11 +28,18 @@ def test_publish_deployment_without_status(rmock):
         ref='release-1',
         environment='production',
         build=1,
-        created_at=datetime.utcnow(),
+        created_at=datetime(2016, 1, 1, 2, 3, 4, 5),
         ci_url='/1',
     )
 
     assert status
+    assert rmock.last_request.json() == {
+        u'auto_merge': False,
+        u'environment': u'production',
+        u'payload': {u'ci_build_id': 1, u'created_at': u'2016-01-01T02:03:04Z'},
+        u'ref': u'release-1',
+        u'required_contexts': []
+    }
 
 
 def test_publish_deployment_with_status(rmock):
@@ -61,6 +68,10 @@ def test_publish_deployment_with_status(rmock):
     )
 
     assert status
+    assert rmock.last_request.json() == {
+        u'state': u'success',
+        u'target_url': u'/1'
+    }
 
 
 def test_dont_publish_if_ref_deployment_already_exists(rmock):
