1724d33a9e5c36169ab79530b8ee853232f3de20
bandesz <bandesz@gmail.com>
2017-02-09 13:47:37 +0000

    Add README link for Terraform, clean up developers policy

--- terraform/modules/iam-common/developers.tf
+++ terraform/modules/iam-common/developers.tf
@@ -6,83 +6,55 @@ resource "aws_iam_policy" "developer" {
   "Statement": [
     {
       "Action": [
-        "sqs:Get*",
-        "sqs:List*",
         "autoscaling:Describe*",
         "cloudwatch:Describe*",
         "cloudwatch:Get*",
         "cloudwatch:List*",
-        "logs:Get*",
-        "logs:FilterLogEvents",
-        "logs:Describe*",
-        "logs:TestMetricFilter",
-        "sns:Get*",
-        "sns:List*",
-        "rds:Describe*",
-        "rds:ListTagsForResource",
+        "ec2:*NetworkAcl*",
+        "ec2:AllocateAddress",
+        "ec2:AssociateAddress",
+        "ec2:AttachVolume",
+        "ec2:CreateVolume",
+        "ec2:DeleteVolume",
         "ec2:Describe*",
-        "elasticloadbalancing:Describe*",
-        "es:Describe*",
-        "es:List*",
-        "s3:ListAllMyBuckets",
+        "ec2:DetachVolume",
+        "ec2:DisassociateAddress",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:RebootInstances",
+        "ec2:ReleaseAddress",
+        "ec2:RunInstances",
+        "ec2:StartInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances",
         "elasticache:Describe*",
         "elasticache:List*",
-        "route53:Get*",
-        "route53:List*",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:Describe*",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "es:Describe*",
+        "es:List*",
         "events:Describe*",
         "events:List*",
         "events:TestEventPattern",
         "kms:DescribeKey",
         "kms:List*",
-        "ec2:RunInstances",
-        "ec2:StartInstances",
-        "ec2:StopInstances",
-        "ec2:RebootInstances",
-        "ec2:TerminateInstances",
-        "ec2:AttachVolume",
-        "ec2:CreateVolume",
-        "ec2:DeleteVolume",
-        "ec2:DetachVolume",
-        "ec2:AllocateAddress",
-        "ec2:AssociateAddress",
-        "ec2:*NetworkAcl*",
-        "ec2:DisassociateAddress",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ReleaseAddress",
-        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-        "elasticloadbalancing:RegisterInstancesWithLoadBalancer"
+        "logs:Describe*",
+        "logs:FilterLogEvents",
+        "logs:Get*",
+        "logs:TestMetricFilter",
+        "rds:Describe*",
+        "rds:ListTagsForResource",
+        "route53:Get*",
+        "route53:List*",
+        "s3:Get*",
+        "s3:List*",
+        "sns:Get*",
+        "sns:List*",
+        "sqs:Get*",
+        "sqs:List*"
       ],
       "Effect": "Allow",
       "Resource": "*",
-      "Condition": {
-        "Bool": {
-          "aws:MultiFactorAuthPresent": true
-        }
-      }
-    },
-    {
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetBucketLocation"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::*"
-      ],
-      "Condition": {
-        "Bool": {
-          "aws:MultiFactorAuthPresent": true
-        }
-      }
-    },
-    {
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::*/*"
-      ],
       "Condition": {
         "Bool": {
           "aws:MultiFactorAuthPresent": true
