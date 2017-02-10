9a1d061d711e8294cbdbe9e17af5a3f2745609db
bandesz <bandesz@gmail.com>
2017-02-10 15:52:42 +0000

    Create admins role in the main account, remove IP restriction from the admin/infra roles

--- terraform/modules/iam-common/admin.tf
+++ terraform/modules/iam-common/admin.tf
@@ -37,3 +37,31 @@ resource "aws_iam_policy" "admin" {
 }
 EOF
 }
+
+resource "aws_iam_role" "admins" {
+  name = "admins"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${var.aws_main_account_id}:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "Bool": {
+          "aws:MultiFactorAuthPresent": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "admins_admin" {
+  role = "${aws_iam_role.admins.name}"
+  policy_arn = "${aws_iam_policy.admin.arn}"
+}
