e5f695326978ed090296caa54d68c96a97b16905
bandesz <bandesz@gmail.com>
2017-02-21 12:23:03 +0000

    Set IAM password policy

--- terraform/modules/iam-common/password_policy.tf
+++ terraform/modules/iam-common/password_policy.tf
@@ -1,8 +1,8 @@
 resource "aws_iam_account_password_policy" "policy" {
-  minimum_password_length = 6
-  require_lowercase_characters = false
-  require_numbers = false
-  require_uppercase_characters = false
+  minimum_password_length = 12
+  require_lowercase_characters = true
+  require_numbers = true
+  require_uppercase_characters = true
   require_symbols = false
   allow_users_to_change_password = true
 }
