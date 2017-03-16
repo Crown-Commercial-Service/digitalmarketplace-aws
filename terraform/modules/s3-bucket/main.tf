d188332100241257de5da741147e44c382c06964
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-03-16 18:06:08 +0000

    Allow all s3 object operations

--- terraform/modules/s3-bucket/main.tf
+++ terraform/modules/s3-bucket/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "s3_bucket" {
   "Statement": [{
     "Effect": "Allow",
     "Principal": {"AWS": "arn:aws:iam::${var.access_account_id}:root"},
-    "Action": "s3:PutObject",
+    "Action": "*",
     "Resource": "arn:aws:s3:::${var.bucket_name}/*"
   }]
 }
