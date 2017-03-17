50dcb8a2510ec9adffe068df80a202815886efa0
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-03-17 18:08:33 +0000

    Remove extra commas from policy JSON

--- terraform/root/aws-dm-dev/s3_buckets.tf
+++ terraform/root/aws-dm-dev/s3_buckets.tf
@@ -6,20 +6,20 @@ resource "aws_s3_bucket" "dev_uploads_s3_bucket" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Principal": {"AWS": "arn:aws:iam::${var.main_account_id}:root"},
+      "Principal": {"AWS": "arn:aws:iam::${var.aws_main_account_id}:root"},
       "Action": [
         "s3:ListBucket",
-        "s3:GetBucketLocation",
+        "s3:GetBucketLocation"
       ],
       "Effect": "Allow",
       "Resource": "arn:aws:s3:::digitalmarketplace-dev-uploads"
     },
     {
-      "Principal": {"AWS": "arn:aws:iam::${var.main_account_id}:root"},
+      "Principal": {"AWS": "arn:aws:iam::${var.aws_main_account_id}:root"},
       "Action": [
         "s3:DeleteObject",
         "s3:GetObject",
-        "s3:PutObject",
+        "s3:PutObject"
       ],
       "Effect": "Allow",
       "Resource": "arn:aws:s3:::digitalmarketplace-dev-uploads/*"
