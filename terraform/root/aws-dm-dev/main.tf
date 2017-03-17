c3d587910a92d5c436b065e512476d76fdf51b9a
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-03-17 18:03:02 +0000

    Remove module from aws-dm-dev

--- terraform/root/aws-dm-dev/main.tf
+++ terraform/root/aws-dm-dev/main.tf
@@ -8,9 +8,3 @@ module "aws_env" {
   aws_main_account_id = "${var.aws_main_account_id}"
   aws_dev_account_id = "${var.aws_dev_account_id}"
 }
-
-module "s3-bucket" {
-  source = "../../modules/s3-bucket"
-  access_account_id = "${var.aws_main_account_id}"
-  bucket_name = "digitalmarketplace-dev-uploads"
-}
