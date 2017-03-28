d4b35c7b2a4154edfcfe6ff96cb5fa473e4c1b64
Alexey Bezhan <al@fueledbylemons.com>
2017-03-28 16:02:02 +0100

    Quote variables in the nginx user_data script
    elasticsearch_auth has a password in arp1 format, which contains
    `$` characters. These are interpreted by bash as variables, so they
    get removed from the final string, which makes nginx complain about
    password mismatch.

--- terraform/modules/nginx/autoscaling.tf
+++ terraform/modules/nginx/autoscaling.tf
@@ -62,26 +62,26 @@ resource "aws_launch_configuration" "nginx" {
   user_data = <<END
 #!/bin/bash
 cd /home/ubuntu/provisioning && ansible-playbook -c local -i localhost, nginx_playbook.yml -t instance-config \
-    -e admin_user_ips=${join(",", var.admin_user_ips)} \
-    -e dev_user_ips=${join(",", var.dev_user_ips)} \
-    -e log_group=${module.logs.log_group_name} \
-    -e json_log_group=${module.json_logs.log_group_name} \
-    -e error_log_group=${module.error_logs.log_group_name} \
-    -e g7_draft_documents_s3_url=${var.g7_draft_documents_s3_url} \
-    -e documents_s3_url=${var.documents_s3_url} \
-    -e agreements_s3_url=${var.agreements_s3_url} \
-    -e communications_s3_url=${var.communications_s3_url} \
-    -e submissions_s3_url=${var.submissions_s3_url} \
-    -e api_url=${var.api_url} \
-    -e search_api_url=${var.search_api_url} \
-    -e buyer_frontend_url=${var.buyer_frontend_url} \
-    -e admin_frontend_url=${var.admin_frontend_url} \
-    -e supplier_frontend_url=${var.supplier_frontend_url} \
-    -e elasticsearch_url=${var.elasticsearch_url} \
-    -e elasticsearch_auth=${var.elasticsearch_auth} \
-    -e app_auth=${var.app_auth} \
-    -e aws_region=${data.aws_region.current.name} \
-    -e nameserver_ip=$(awk '/nameserver/{ print $2; exit}' /etc/resolv.conf)
+    -e admin_user_ips='${join(",", var.admin_user_ips)}' \
+    -e dev_user_ips='${join(",", var.dev_user_ips)}' \
+    -e log_group='${module.logs.log_group_name}' \
+    -e json_log_group='${module.json_logs.log_group_name}' \
+    -e error_log_group='${module.error_logs.log_group_name}' \
+    -e g7_draft_documents_s3_url='${var.g7_draft_documents_s3_url}' \
+    -e documents_s3_url='${var.documents_s3_url}' \
+    -e agreements_s3_url='${var.agreements_s3_url}' \
+    -e communications_s3_url='${var.communications_s3_url}' \
+    -e submissions_s3_url='${var.submissions_s3_url}' \
+    -e api_url='${var.api_url}' \
+    -e search_api_url='${var.search_api_url}' \
+    -e buyer_frontend_url='${var.buyer_frontend_url}' \
+    -e admin_frontend_url='${var.admin_frontend_url}' \
+    -e supplier_frontend_url='${var.supplier_frontend_url}' \
+    -e elasticsearch_url='${var.elasticsearch_url}' \
+    -e elasticsearch_auth='${var.elasticsearch_auth}' \
+    -e app_auth='${var.app_auth}' \
+    -e aws_region='${data.aws_region.current.name}' \
+    -e nameserver_ip="$(awk '/nameserver/{ print $2; exit}' /etc/resolv.conf)"
 END
 
   lifecycle {
