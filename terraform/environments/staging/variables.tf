dda117e16c4ca5afe9b8793a65b0925237b21cd6
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-03-30 10:53:49 +0100

    Create staging environment

---
+++ terraform/environments/staging/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_main_account_id" {}
+variable "admin_user_ips" {
+  type = "list"
+}
+variable "dev_user_ips" {
+  type = "list"
+}
+variable "user_ips" {
+  type = "list"
+}
+
+variable "ssh_key_name" {}
+
+variable "g7_draft_documents_s3_url" {}
+variable "documents_s3_url" {}
+variable "agreements_s3_url" {}
+variable "communications_s3_url" {}
+variable "submissions_s3_url" {}
+
+variable "api_url" {}
+variable "search_api_url" {}
+variable "buyer_frontend_url" {}
+variable "admin_frontend_url" {}
+variable "supplier_frontend_url" {}
+
+variable "elasticsearch_auth" {}
+variable "app_auth" {}
