2a8566bfda9bbeed5737f4ad2eac03e923093e9c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-05-09 15:57:51 +0100

    Add module for setting up nginx/app log groups for apps
    The apps for each environment need to have 2 log groups each, one for
    their application logs, and the other for their nginx logs.

---
+++ terraform/modules/application-logs/variables.tf
@@ -0,0 +1,6 @@
+variable "environment" {}
+variable "retention_in_days" {}
+variable "app_names" {
+  type = "list"
+  default = ["buyer-frontend", "supplier-frontend", "admin-frontend", "api", "search-api"]
+}
