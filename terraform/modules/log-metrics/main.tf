1918c3e1a08f0417c2e320a7af5febac4b220146
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2017-07-07 13:53:52 +0100

    Loop over the apps instead of buckets

--- terraform/modules/log-metrics/main.tf
+++ terraform/modules/log-metrics/main.tf
@@ -1,93 +1,140 @@
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-api" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-api-request-times-${count.index}"
+# App-specific nginx log buckets
 
-  # The following resolves to something like:
-  # { $.requestTime >= 0.1 && $.requestTime < 0.2 }
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\"}"
-  log_group_name = "${var.environment}-api-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_0" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-0"
+
+  pattern        = "{$$.requestTime >= 0 && $$.requestTime < 0.025 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-api-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-0"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-search-api" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-search-api-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\"}"
-  log_group_name = "${var.environment}-search-api-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_1" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-1"
+
+  pattern        = "{$$.requestTime >= 0.025 && $$.requestTime < 0.05 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-search-api-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-1"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-buyer-frontend" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-buyer-frontend-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
-  log_group_name = "${var.environment}-buyer-frontend-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_2" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-2"
+
+  pattern        = "{$$.requestTime >= 0.05 && $$.requestTime < 0.1 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-buyer-frontend-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-2"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-supplier-frontend" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-supplier-frontend-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
-  log_group_name = "${var.environment}-supplier-frontend-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_3" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-3"
+
+  pattern        = "{$$.requestTime >= 0.1 && $$.requestTime < 0.25 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-supplier-frontend-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-3"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-admin-frontend" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-admin-frontend-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
-  log_group_name = "${var.environment}-admin-frontend-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_4" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-4"
+
+  pattern        = "{$$.requestTime >= 0.25 && $$.requestTime < 0.5 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-admin-frontend-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-4"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-briefs-frontend" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-briefs-frontend-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
-  log_group_name = "${var.environment}-briefs-frontend-nginx"
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_5" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-5"
+
+  pattern        = "{$$.requestTime >= 0.5 && $$.requestTime < 1 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-briefs-frontend-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-5"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
 
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_6" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-6"
 
-resource "aws_cloudwatch_log_metric_filter" "requestTimeBuckets-main-nginx" {
-  count = "${length(var.request_time_buckets)}"
-  name  = "${var.environment}-nginx-request-times-${count.index}"
-  pattern        = "{$$.requestTime >= ${lookup(var.request_time_buckets[count.index], "min")} && $$.requestTime < ${lookup(var.request_time_buckets[count.index], "max")} && $$.request != \"*/_status?ignore-dependencies *\"}"
-  log_group_name = "${var.environment}-nginx-json"
+  pattern        = "{$$.requestTime >= 1  && $$.requestTime < 2.5 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
 
   metric_transformation {
-    name  = "${var.environment}-nginx-request-times-${count.index}"
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-6"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_7" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-7"
+
+  pattern        = "{$$.requestTime >= 2.5  && $$.requestTime < 5 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
+
+  metric_transformation {
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-7"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_8" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-8"
+
+  pattern        = "{$$.requestTime >= 5  && $$.requestTime < 10 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
+
+  metric_transformation {
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-8"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_9" {
+  count = "${length(var.app_names)}"
+  name  = "${var.environment}-${var.app_names[count.index]}-request-times-9"
+
+  pattern        = "{$$.requestTime >= 10 && $$.request != \"*/_status?ignore-dependencies *\" && $$.request != \"*/static/*\"}"
+  log_group_name = "${var.environment}-${var.app_names[count.index]}-nginx"
+
+  metric_transformation {
+    name  = "${var.environment}-${var.app_names[count.index]}-request-times-9"
     namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
@@ -106,14 +153,124 @@ resource "aws_cloudwatch_log_metric_filter" "application-500s" {
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "main-nginx-500s" {
-  name  = "${var.environment}-nginx-500s"
-  pattern        = "{$$.status = 5*}"
+# Main nginx log buckets
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_0" {
+  name  = "${var.environment}-nginx-request-times-0"
+  pattern        = "{$$.requestTime >= 0 && $$.requestTime < 0.025 && $$.request != \"*/_status?ignore-dependencies *\"}"
   log_group_name = "${var.environment}-nginx-json"
 
   metric_transformation {
-    name  = "${var.environment}-nginx-500s"
-    namespace = "DM-500s"
+    name  = "${var.environment}-nginx-request-times-0"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_1" {
+  name  = "${var.environment}-nginx-request-times-1"
+  pattern        = "{$$.requestTime >= 0.025 && $$.requestTime < 0.05 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-1"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_2" {
+  name  = "${var.environment}-nginx-request-times-2"
+  pattern        = "{$$.requestTime >= 0.05 && $$.requestTime < 0.1 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-2"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_3" {
+  name  = "${var.environment}-nginx-request-times-3"
+  pattern        = "{$$.requestTime >= 0.1 && $$.requestTime < 0.25 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-3"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_4" {
+  name  = "${var.environment}-nginx-request-times-4"
+  pattern        = "{$$.requestTime >= 0.25 && $$.requestTime < 0.5 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-4"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_5" {
+  name  = "${var.environment}-nginx-request-times-5"
+  pattern        = "{$$.requestTime >= 0.5 && $$.requestTime < 1 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-5"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_6" {
+  name  = "${var.environment}-nginx-request-times-6"
+  pattern        = "{$$.requestTime >= 1 && $$.requestTime < 2.5 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-6"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_7" {
+  name  = "${var.environment}-nginx-request-times-7"
+  pattern        = "{$$.requestTime >= 2.5 && $$.requestTime < 5 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-7"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_8" {
+  name  = "${var.environment}-nginx-request-times-8"
+  pattern        = "{$$.requestTime >= 5 && $$.requestTime < 10 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-8"
+    namespace = "DM-RequestTimeBuckets"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "request_time_bucket_9" {
+  name  = "${var.environment}-nginx-request-times-9"
+  pattern        = "{$$.requestTime >= 10 && $$.request != \"*/_status?ignore-dependencies *\"}"
+  log_group_name = "${var.environment}-nginx-json"
+
+  metric_transformation {
+    name  = "${var.environment}-nginx-request-times-9"
+    namespace = "DM-RequestTimeBuckets"
     value     = "1"
   }
 }
