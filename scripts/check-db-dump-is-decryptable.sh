3c1817216ad9a73f1d43a60a511344401b63739f
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-07-25 15:43:18 +0100

    Add loopback pinentry to gpg-agent conf
    To allow for a passphrase to be passed in to GPG via stdin, the
    allow-loopback-pinentry option needs to be set. See here for more info:
    https://wiki.archlinux.org/index.php/GnuPG#Unattended_passphrase

--- scripts/check-db-dump-is-decryptable.sh
+++ scripts/check-db-dump-is-decryptable.sh
@@ -3,7 +3,7 @@
 LATEST_DUMP=$(aws s3 ls s3://digitalmarketplace-database-backups | sort | tail -n 1 | awk '{print $4}')
 aws s3 cp s3://digitalmarketplace-database-backups/${LATEST_DUMP} ./latest-dump.sql.gz.gpg
 gpg2 --batch --import <($DM_CREDENTIALS_REPO/sops-wrapper -d $DM_CREDENTIALS_REPO/gpg/database-backups/secret.key.enc)
-echo -n $($DM_CREDENTIALS_REPO/sops-wrapper -d $DM_CREDENTIALS_REPO/gpg/database-backups/secret-key-passphrase.txt.enc) | gpg2 --batch --passphrase-fd 0 --list-packets ./latest-dump.sql.gz.gpg
+echo -n $($DM_CREDENTIALS_REPO/sops-wrapper -d $DM_CREDENTIALS_REPO/gpg/database-backups/secret-key-passphrase.txt.enc) | gpg2 --batch --passphrase-fd 0 --pinentry-mode loopback --list-packets ./latest-dump.sql.gz.gpg
 EXIT_CODE=$?
 
 if [ ${EXIT_CODE} -gt 0 ]; then
