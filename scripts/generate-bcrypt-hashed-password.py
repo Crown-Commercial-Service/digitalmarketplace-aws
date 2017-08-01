29596d27611e950be2c31d4fb75c32f5d69dd2ec
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-01 10:50:17 +0100

    Add script to generated bcrypt hashed passwords.
    Python has a good bcrypt library. This script is a wrapper for password
    hashing and allows you to pass in the password to be hashed as well as
    cost-factor.

---
+++ scripts/generate-bcrypt-hashed-password.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+A script to return a bcrypt hash of a password.
+
+It's intended use is for creating known passwords to replace user passwords in cleaned up databases.
+
+Cost-factor is the log2 number of rounds of hashing to use for the salt. It's worth researching how many rounds you need
+for your use context, but recent recommendations are 10-12 as a minimum.
+
+Usage:
+scripts/generate-bcrpyt-hashed-password.py <password> <cost-factor>
+"""
+
+import bcrypt
+from docopt import docopt
+
+
+def hash_password(password, cost_factor):
+    hashed_password = bcrypt.hashpw(
+        bytes(password),
+        bcrypt.gensalt(cost_factor)
+    ).decode('utf-8')
+    return hashed_password
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    password = arguments['<password>']
+    cost_factor = int(arguments['<cost-factor>'])
+    print(hash_password(password, cost_factor))
