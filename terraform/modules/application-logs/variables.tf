b0a8a4a7c15eee9ccdb1b3a9d877d987cf983564
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-16 16:38:17 +0100

    Add brief-responses-frontend to list of apps in terraform
    Adding the new app to the list in the application logs module will
    create the log groups as well as the metrics.

--- terraform/modules/application-logs/variables.tf
+++ terraform/modules/application-logs/variables.tf
@@ -2,5 +2,13 @@ variable "environment" {}
 variable "retention_in_days" {}
 variable "app_names" {
   type = "list"
-  default = ["buyer-frontend", "supplier-frontend", "admin-frontend", "api", "search-api", "briefs-frontend"]
+  default = [
+    "buyer-frontend",
+    "supplier-frontend",
+    "admin-frontend",
+    "api",
+    "search-api",
+    "briefs-frontend",
+    "brief-responses-frontend"
+  ]
 }
