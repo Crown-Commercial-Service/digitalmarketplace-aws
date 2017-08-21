1a42500169377446d8e864e6f14765481a27372d
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2017-08-21 13:12:55 +0100

    routing for buyer-frontend with direct-award; tweak templating to allow multiple paths. The first path supplied is implicitly the path to use for the healthcheck.

--- scripts/unmap-route.sh
+++ scripts/unmap-route.sh
@@ -8,13 +8,16 @@ fi
 APPLICATION_NAME=$1
 APP_GUID=$(cf app --guid ${APPLICATION_NAME})
 
-ROUTE_URL=$(cf curl /v2/apps/${APP_GUID}/route_mappings | jq -r '.resources[0].entity.route_url')
+ROUTE_URLS=$(cf curl /v2/apps/${APP_GUID}/route_mappings | jq -r '.resources[].entity.route_url')
 
-ROUTE_DATA=$(cf curl ${ROUTE_URL} | jq '.entity')
+for ROUTE_URL in ${ROUTE_URLS}; do
+  ROUTE_DATA=$(cf curl ${ROUTE_URL} | jq '.entity')
 
-ROUTE_HOST=$(echo $ROUTE_DATA | jq -r '.host')
-ROUTE_PATH=$(echo $ROUTE_DATA | jq -r '.path')
+  ROUTE_HOST=$(echo $ROUTE_DATA | jq -r '.host')
+  ROUTE_PATH=$(echo $ROUTE_DATA | jq -r '.path')
 
-echo "Unmapping ${ROUTE_HOST}.cloudapps.digital/${ROUTE_PATH} from ${APPLICATION_NAME}"
+  echo "Unmapping ${ROUTE_HOST}.cloudapps.digital/${ROUTE_PATH} from ${APPLICATION_NAME}"
 
-cf unmap-route "${APPLICATION_NAME}" cloudapps.digital --hostname "${ROUTE_HOST}" --path "${ROUTE_PATH}"
+  cf unmap-route "${APPLICATION_NAME}" cloudapps.digital --hostname "${ROUTE_HOST}" --path "${ROUTE_PATH}"
+  done
+done
