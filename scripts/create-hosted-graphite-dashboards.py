adce459711db611b0386c01e87b1c5ff5413cb8f
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-12-22 16:20:07 +0000

    Use absolute path to grafana directory in script
    So it can be run from the root of the repo. Or anywhere.

--- scripts/create-hosted-graphite-dashboards.py
+++ scripts/create-hosted-graphite-dashboards.py
@@ -22,7 +22,7 @@ sys.path.insert(0, '.')  # noqa
 
 def generate_dashboards(api_key):
     endpoint = "https://api.hostedgraphite.com/api/v2/grafana/dashboards/"
-    path = "../grafana/"
+    path = os.path.join(os.path.dirname(__file__), "../grafana/")
 
     for filename in os.listdir(path):
         with open(path + filename) as fp:
