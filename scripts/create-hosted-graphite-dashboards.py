64dd3ecb1cf5adbf68d59d231b67e7d30ace715f
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-12-22 16:53:27 +0000

    Update docstring for creating hosted graphite dashboards

--- scripts/create-hosted-graphite-dashboards.py
+++ scripts/create-hosted-graphite-dashboards.py
@@ -2,7 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Description:
-    To add
+    This pushes dashboards defined in /grafana directory up to our hosted graphite.
+    You can get the <hosted_graphite_api_key> from the "Account Home" page of hosted graphite.
+
+    To change a dashboard a process you should follow is:
+    1. Set "editable": true in one our existing JSON files
+    2. Change the title (to avoid overwriting the existing dashboard as you try things out)
+    3. Run this script
+    4. Find the newly created dashboard in hosted graphite and make changes
+    5. Export the JSON for the new dashboard (click settings cog, "View JSON") and overwrite the existing JSON file
+    6. Set "editable": false and set the title back to the original one.  Remove the new "id" key from the exported JSON
+    7. Run this script again
+    8. Check you're happy with the new dashboard
+    9. Delete the editable "test" dashboard you created in step 3 (click settings cog, "Delete dashboard")
 
 Usage:
     scripts/create-hosted-graphite-dashboards.py <hosted_graphite_api_key>
