843aefa5abfb3fd6b90d5c00ee5c9c1d09bb1add
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-03-05 12:52:30 +0000

    Add Jenkins_2
    This adds the Terraform code to spin up a new instance for a new Jenkins
    box. This is being done to allow pen testers to test a Jenkins instance
    that isn't our actual instance. It will be provisioned via Ansiible to
    be a similar as we can get it.
    
    This Terraform code uses the existing security group that is applied to
    our current box, and the same instance profile.
    
    There is an extra security group applied, which is to allow access to
    the box from Cyberis's IP's.
    
    This could lay the groundwork for a new, upgraded Jenkins box which
    could be built in the future. Alternatively, this Terraform could be
    scrapped and we could move Jenkins to the PaaS.

--- terraform/modules/jenkins/variables.tf
+++ terraform/modules/jenkins/variables.tf
@@ -3,3 +3,8 @@ variable "aws_main_account_id" {}
 variable "aws_sub_account_ids" {
   type = "list"
 }
+variable "jenkins_security_group_ids" {
+  type = "list"
+}
+variable "jenkins_2_key_name" {}
+variable "jenkins_2_public_key" {}
