43e8657d2d7be1cf0e2ded48f00a020a51875333
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-04-19 08:25:11 +0100

    Enforce formatting on terraform files
     ## Summary
    Enforce consistent formatting of Terraform configuration files through
    Travis.

--- Makefile
+++ Makefile
@@ -19,6 +19,10 @@ help:
 .PHONY: test
 test: test-flake8 test-unit
 
+.PHONY: terraformat
+terraformat:
+	./scripts/check-terraform-formatting.sh
+
 .PHONY: test-flake8
 test-flake8: virtualenv
 	${VIRTUALENV_ROOT}/bin/flake8 .
---
+++ scripts/check-terraform-formatting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LINT_OUTPUT=$(terraform fmt -write=false -list=true -diff=true terraform)
+if [ ! -z "${LINT_OUTPUT}" ]; then
+  echo "${LINT_OUTPUT}"
+  echo "Terraform formatting check has FAILED. Fix formatting issues with \`terraform fmt -write=true -list=true -diff=true terraform\` from the root of the repository."
+  exit 1
+fi
+
+echo "Terraform formatting check has PASSED."
