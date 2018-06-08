94b5a80902c75f295a4cc57c2a2292dfde03211e
benvand <benjaminfvandersteen@gmail.com>
2018-06-08 13:42:32 +0100

    Extrapolate terraform creation out of main file into dedicated terraform file

--- terraform/accounts/backups/main.tf
+++ terraform/accounts/backups/main.tf
@@ -11,30 +11,3 @@ terraform {
     encrypt = "true"
   }
 }
-
-module "gds-audit-cloudtrail" {
-  source               = "../../modules/cloudtrail/cloudtrail"
-  trail_name           = "digitalmarketplace-gds-audit-cloudtrail"
-  s3_bucket_name       = "gds-audit-cloudtrails"                   // This is currently the name of the RE bucket being used to log GDS sub accounts
-  s3_bucket_key_prefix = "${var.aws_backups_account_id}"           // As per RE guidelines this should be the id of the account exporting the CloudTrail trail log files
-}
-
-module "cloudtrail-bucket" {
-  source         = "../../modules/cloudtrail/cloudtrail-bucket"
-  s3_bucket_name = "digitalmarketplaces-backups-account-cloudtrail-bucket"
-}
-
-module "cloudtrail-cloudwatch" {
-  source            = "../../modules/cloudtrail/cloudtrail-cloudwatch"
-  trail_name        = "digitalmarketplace-backups-account-cloudtrail"
-  retention_in_days = 731                                              // As per RE convention on keeping logs for 2 years
-}
-
-module "cloudtrail" {
-  source                     = "../../modules/cloudtrail/cloudtrail"
-  trail_name                 = "digitalmarketplaces-backups-account-cloudtrail"
-  s3_bucket_name             = "${module.cloudtrail-bucket.s3_bucket_name}"
-  s3_bucket_key_prefix       = "${var.aws_backups_account_id}"
-  cloud_watch_logs_role_arn  = "${module.cloudtrail-cloudwatch.cloud_watch_logs_role_arn}"
-  cloud_watch_logs_group_arn = "${module.cloudtrail-cloudwatch.cloud_watch_logs_group_arn}"
-}
