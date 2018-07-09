9c5e5864f7f517f81f708fe701c9aa82a7f07d33
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-07-09 11:02:57 +0100

    Remove SW, GL, PT, DH, AC, LB

--- terraform/modules/iam-users/users/main.tf
+++ terraform/modules/iam-users/users/main.tf
@@ -1,15 +1,5 @@
-resource "aws_iam_user" "andrew_chong" {
-  name          = "andrewchong"
-  force_destroy = true
-}
-
 resource "aws_iam_user" "ben_vandersteen" {
   name          = "benvandersteen"
-  force_destroy = true
-}
-
-resource "aws_iam_user" "brendan_barber" {
-  name          = "brendanbutler"
   force_destroy = true
 }
 
@@ -18,23 +8,8 @@ resource "aws_iam_user" "chris_wynne" {
   force_destroy = true
 }
 
-resource "aws_iam_user" "dilwoar_hussain" {
-  name          = "dilwoarhussain"
-  force_destroy = true
-}
-
-resource "aws_iam_user" "george_lund" {
-  name          = "georgelund"
-  force_destroy = true
-}
-
 resource "aws_iam_user" "kat_stevens" {
   name          = "katstevens"
-  force_destroy = true
-}
-
-resource "aws_iam_user" "laurence_berry" {
-  name          = "laurenceberry"
   force_destroy = true
 }
 
@@ -43,18 +18,8 @@ resource "aws_iam_user" "louise_ryan" {
   force_destroy = true
 }
 
-resource "aws_iam_user" "pea_tyczynska" {
-  name          = "peatyczynska"
-  force_destroy = true
-}
-
 resource "aws_iam_user" "robert_scott" {
   name          = "robertscott"
-  force_destroy = true
-}
-
-resource "aws_iam_user" "samuel_williams" {
-  name          = "samuelwilliams"
   force_destroy = true
 }
 
