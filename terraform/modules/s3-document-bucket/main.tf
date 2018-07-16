629bb35e440a581776dc7d85406a14caf6b36b4c
benvand <benjaminfvandersteen@gmail.com>
2018-07-16 15:57:37 +0100

    Add new module for creating simple document buckets with read, write and list perms

---
+++ terraform/modules/s3-document-bucket/main.tf
@@ -0,0 +1,73 @@
+data "aws_iam_policy_document" "document_bucket_policy_document" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = "${var.read_object_roles}"
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}/*",
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = "${var.write_object_roles}"
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+    ]
+
+    resources = [
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}/*",
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = "${var.list_bucket_roles}"
+      type        = "AWS"
+    }
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}/*",
+      "arn:aws:s3:::digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}",
+    ]
+  }
+}
+
+resource "aws_s3_bucket" "document_bucket" {
+  bucket = "digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "${var.log_bucket_name}"
+    target_prefix = "digitalmarketplace-${var.bucket_name}-${var.environment}-${var.environment}/"
+  }
+
+  policy = "${data.aws_iam_policy_document.document_bucket_policy_document.json}"
+}
---
+++ terraform/modules/s3-document-bucket/variables.tf
@@ -0,0 +1,15 @@
+variable "read_object_roles" {
+  type = "list"
+}
+
+variable "write_object_roles" {
+  type = "list"
+}
+
+variable "list_bucket_roles" {
+  type = "list"
+}
+
+variable "bucket_name" {}
+variable "environment" {}
+variable "log_bucket_name" {}
