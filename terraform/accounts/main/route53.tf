9350c8de37edcb3bbd513c241445b55076d78373
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-08-22 15:23:41 +0100

    Remove old Jenkins Terraform
    The old security group was only used by the old box which has been
    destroyed. The new box has it's security group defined in the new
    module.
    
    We can drop the A record which pointed at the old jenkins ip as that
    elastic ip has been removed.

--- terraform/accounts/main/route53.tf
+++ terraform/accounts/main/route53.tf
@@ -30,17 +30,6 @@ resource "aws_route53_record" "staging_ns" {
   ]
 }
 
-resource "aws_route53_record" "ci_marketplace_team" {
-  zone_id = "${aws_route53_zone.marketplace_team.zone_id}"
-  name    = "ci.marketplace.team"
-  type    = "A"
-  ttl     = "300"
-
-  records = [
-    "${var.jenkins_ip}",
-  ]
-}
-
 resource "aws_route53_record" "ci3_marketplace_team" {
   zone_id = "${aws_route53_zone.marketplace_team.zone_id}"
   name    = "ci3.marketplace.team"
