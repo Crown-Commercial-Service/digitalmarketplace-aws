2e1e5c3d1810bb8e0ec914c756c82b5eafef7b05
benvand <benjaminfvandersteen@gmail.com>
2018-09-17 09:27:13 +0100

    Remove unused variables
    It would appear that these have been at least partially copied from account/main/vairiables.tf?
    
    We don't need that many variables injected in to these terraform modules so this commit strips un-needed variables
    to reduce confusion and prevent extraneous code.

--- terraform/environments/production/variables.tf
+++ terraform/environments/production/variables.tf
@@ -1,34 +1,8 @@
 variable "aws_main_account_id" {}
 variable "aws_prod_account_id" {}
 
-variable "admin_user_ips" {
-  type = "list"
-}
-
-variable "dev_user_ips" {
-  type = "list"
-}
-
-variable "user_ips" {
-  type = "list"
-}
-
-variable "ssh_key_name" {}
-
-variable "g7_draft_documents_s3_url" {}
-variable "documents_s3_url" {}
-variable "agreements_s3_url" {}
-variable "communications_s3_url" {}
-variable "reports_s3_url" {}
-variable "submissions_s3_url" {}
-
-variable "api_url" {}
-variable "search_api_url" {}
-variable "frontend_url" {}
 variable "antivirus_api_host" {}
 variable "antivirus_api_basic_auth" {}
-
-variable "app_auth" {}
 
 variable "logs_elasticsearch_url" {}
 variable "logs_elasticsearch_api_key" {}
