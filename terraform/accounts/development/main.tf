d6b17a1b7b3ae97a2ec10baa4d8544fad944a863
benvand <benjaminfvandersteen@gmail.com>
2018-09-17 14:50:38 +0100

    565-remove-aws-env-module-for-terraform-to-read-more-clearly
    The aws-env module is just a shortcut that calls aws-common, switch-roles and paas.
    It is currently called in the development and production accounts.
    
    The aws-common module is also called independently in the main account.
    
    Removing the shortcut `aws-env` means that it should be more obvious that`aws-common` is called everywhere and `paas` and `switch-roles` are only called in development and production.
    
    Remove:
    
    shortcut module aws-env:
    iam-common()
    paas()
    switch-roles()
    
    account main:
    iam-common()
    
    account production:
    aws-env()
    
    account development:
    aws-env()
    
    Add:
    
    account main:
    iam-common()
    
    account production:
    iam-common()
    paas()
    switch-roles()
    
    account development:
    iam-common()
    paas()
    switch-roles()

--- terraform/accounts/development/main.tf
+++ terraform/accounts/development/main.tf
@@ -16,9 +16,20 @@ terraform {
   }
 }
 
-module "aws_env" {
-  source              = "../../modules/aws-env"
+module "iam_common" {
+  source              = "../../modules/iam-common"
   dev_user_ips        = "${var.dev_user_ips}"
   aws_main_account_id = "${var.aws_main_account_id}"
   aws_dev_account_id  = "${var.aws_dev_account_id}"
+}
+
+module "switch_roles" {
+  source                          = "../../modules/switch-roles"
+  ip_restricted_access_policy_arn = "${module.iam_common.aws_iam_policy_ip_restricted_access_arn}"
+  iam_manage_account_policy_arn   = "${module.iam_common.aws_iam_policy_iam_manage_account_arn}"
+  aws_main_account_id             = "${var.aws_main_account_id}"
+}
+
+module "paas" {
+  source = "../../modules/paas"
 }
--- terraform/modules/aws-env/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "dev_user_ips" {
-  type = "list"
-}
-
-variable "aws_main_account_id" {}
-variable "aws_dev_account_id" {}
