e06def6f4c36f713580274f7f574d9957a516f9a
benvand <benjaminfvandersteen@gmail.com>
2018-09-17 15:06:42 +0100

    Remove arn:aws:iam::${var.aws_dev_account_id}:role/s3-only references from terraform. This role is not created anywhere
    We have references to an s3-only role for the dev account. This isn't created anywhere and doesn't exist.
    
    arn:aws:iam::${var.aws_dev_account_id}:role/s3-only

--- terraform/modules/iam-common/admin.tf
+++ terraform/modules/iam-common/admin.tf
@@ -14,25 +14,6 @@ resource "aws_iam_policy" "admin" {
           "aws:MultiFactorAuthPresent": true
         }
       }
-    },
-    {
-      "Effect": "Allow",
-      "Action": "sts:AssumeRole",
-      "Resource": [
-        "arn:aws:iam::${var.aws_dev_account_id}:role/s3-only"
-      ]
-    },
-    {
-      "Effect": "Deny",
-      "Action": "sts:AssumeRole",
-      "NotResource": [
-        "arn:aws:iam::${var.aws_dev_account_id}:role/s3-only"
-      ],
-      "Condition": {
-        "BoolIfExists": {
-          "aws:MultiFactorAuthPresent": false
-        }
-      }
     }
   ]
 }
