364375caa263f1c8dd3e7dfaeeb6019315e4c299
benvand <benjaminfvandersteen@gmail.com>
2018-09-25 12:41:35 +0100

    Add temporary users to development and production environments to use whilst accounts are being migrated between aws-env and accounts dir

--- terraform/accounts/development/main.tf
+++ terraform/accounts/development/main.tf
@@ -33,3 +33,88 @@ module "switch_roles" {
 module "paas" {
   source = "../../modules/paas"
 }
+
+### TEMPORARY PAAS USER
+
+resource "aws_iam_user" "paas_app" {
+  name = "paas-app-TEMP"
+}
+
+resource "aws_iam_user_policy" "paas_app_policy" {
+  user = "${aws_iam_user.paas_app.name}"
+  name = "PaaSAppPolicy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:Get*",
+        "s3:List*",
+        "s3:Put*",
+        "s3:DeleteObject"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams",
+        "logs:DescribeLogGroups"
+      ],
+      "Resource": "arn:aws:logs:eu-west-1:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "cloudwatch:PutMetricData"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+### TEMPORARY PAAS METRICS USER
+
+resource "aws_iam_user" "paas_metrics_collector" {
+  name = "paas-metrics-collector-TEMP"
+}
+
+resource "aws_iam_user_policy" "grafana" {
+  user = "${aws_iam_user.paas_metrics_collector.name}"
+  name = "Grafana"
+
+  policy = <<EOF
+{
+    "Statement": [
+        {
+            "Sid": "PermissionsForMetrics",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:ListMetrics",
+                "cloudwatch:GetMetricStatistics",
+                "ec2:DescribeInstances",
+                "ec2:DescribeVolumes"
+            ],
+            "Resource": [ "*" ]
+        },
+        {
+            "Sid": "PermissionsForTags",
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:DescribeTags"
+            ],
+            "Resource": [ "*" ]
+        }
+    ],
+    "Version": "2012-10-17"
+}
+EOF
+}
