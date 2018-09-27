7f0a7129f59e44e6cb4e185dee860d918e0a2a0f
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-09-27 12:09:18 +0100

    Replication roles and policies for S3 document buckets

---
+++ terraform/accounts/development/access.tf
@@ -0,0 +1,103 @@
+# Replication role, policy and role-policy attachment
+# Only S3 service can assume this role and perform these actions.
+# (See https://www.terraform.io/docs/providers/aws/r/s3_bucket.html#using-replication-configuration)
+resource "aws_iam_role" "replication_role" {
+  name = "replication"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "replication_policy" {
+  name = "replication-policy"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-documents-preview-preview"
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-documents-preview-preview/*"
+    },
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-agreements-preview-preview"
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-agreements-preview-preview/*"
+    },
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-communications-preview-preview"
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-communications-preview-preview/*"
+    },
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-submissions-preview-preview"
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::digitalmarketplace-submissions-preview-preview/*"
+    },
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "replication" {
+  role       = "${aws_iam_role.replication_role.id}"
+  policy_arn = "${aws_iam_policy.replication_policy.arn}"
+}
