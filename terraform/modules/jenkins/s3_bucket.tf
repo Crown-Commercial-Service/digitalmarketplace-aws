a9973a230248793a5301d23ee2cfec4e9775c46f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2018-11-30 11:37:23 +0000

    Add comment elucidating ELB account ID in Jenkins instance s3 bucket policy

--- terraform/modules/jenkins/s3_bucket.tf
+++ terraform/modules/jenkins/s3_bucket.tf
@@ -2,6 +2,9 @@ resource "aws_s3_bucket" "jenkins_logs_bucket" {
   bucket        = "${var.name}-${var.dns_name}-logs-bucket"
   force_destroy = true
 
+  # The id 156460612806 is the ELB account (eu-west-1)
+  # see https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-access-logs.html
+
   policy = <<POLICY
 {
     "Version": "2012-10-17",
