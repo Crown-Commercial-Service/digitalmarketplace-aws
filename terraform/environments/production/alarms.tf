67f1b23a2a3cfdeb2eae763a4592f3b6c176df88
benvand <benjaminfvandersteen@gmail.com>
2018-12-19 16:00:21 +0000

    Add alarms to production environment

--- terraform/environments/production/alarms.tf
+++ terraform/environments/production/alarms.tf
@@ -1,22 +1,22 @@
 module "email_alarm_sns" {
   source      = "../../modules/logging/alarms/email-notification"
-  environment = "preview"
-  account_id  = "${var.aws_dev_account_id}"
+  environment = "production"
+  account_id  = "${var.aws_prod_account_id}"
 }
 
-// Missing logs for the preview-<app-name>-application log group metrics
+// Missing logs for the production-<app-name>-application log group metrics
 module "missing_application_logs_alarms" {
   source                = "../../modules/logging/alarms/missing-logs"
-  environment           = "preview"
+  environment           = "production"
   type                  = "application"
   app_names             = ["${module.application_logs.app_names}"]
   alarm_email_topic_arn = "${module.email_alarm_sns.alarm_email_topic_arn}"
 }
 
-// Missing logs for the preview-router-nginx log group metrics
+// Missing logs for the production-router-nginx log group metrics
 module "missing_nginx_logs_alarms" {
   source                = "../../modules/logging/alarms/missing-logs"
-  environment           = "preview"
+  environment           = "production"
   type                  = "nginx"
   app_names             = ["router"]
   alarm_email_topic_arn = "${module.email_alarm_sns.alarm_email_topic_arn}"
@@ -24,21 +24,21 @@ module "missing_nginx_logs_alarms" {
 
 module "slow_requests_alarms" {
   source                = "../../modules/logging/alarms/slow-requests"
-  environment           = "preview"
+  environment           = "production"
   app_name              = "router"
   alarm_email_topic_arn = "${module.email_alarm_sns.alarm_email_topic_arn}"
 }
 
 module "router_500_alarm" {
   source                = "../../modules/logging/alarms/status-code"
-  environment           = "preview"
+  environment           = "production"
   status_code           = "500"
   alarm_email_topic_arn = "${module.email_alarm_sns.alarm_email_topic_arn}"
 }
 
 module "router_429_alarm" {
   source                = "../../modules/logging/alarms/status-code"
-  environment           = "preview"
+  environment           = "production"
   status_code           = "429"
   alarm_email_topic_arn = "${module.email_alarm_sns.alarm_email_topic_arn}"
 }
