89e6144c2374a7e345a98249e4a7dda974fde31e
benvand <benjaminfvandersteen@gmail.com>
2019-02-13 14:16:15 +0000

    We no longer export metrics using these users

--- terraform/modules/paas/iam.tf
+++ terraform/modules/paas/iam.tf
@@ -42,39 +42,3 @@ resource "aws_iam_user_policy" "paas_app_policy" {
 }
 EOF
 }
-
-resource "aws_iam_user" "paas_metrics_collector" {
-  name = "paas-metrics-collector"
-}
-
-resource "aws_iam_user_policy" "grafana" {
-  user = "${aws_iam_user.paas_metrics_collector.name}"
-  name = "Grafana"
-
-  policy = <<EOF
-{
-    "Statement": [
-        {
-            "Sid": "PermissionsForMetrics",
-            "Effect": "Allow",
-            "Action": [
-                "cloudwatch:ListMetrics",
-                "cloudwatch:GetMetricStatistics",
-                "ec2:DescribeInstances",
-                "ec2:DescribeVolumes"
-            ],
-            "Resource": [ "*" ]
-        },
-        {
-            "Sid": "PermissionsForTags",
-            "Effect": "Allow",
-            "Action": [
-                "elasticloadbalancing:DescribeTags"
-            ],
-            "Resource": [ "*" ]
-        }
-    ],
-    "Version": "2012-10-17"
-}
-EOF
-}
