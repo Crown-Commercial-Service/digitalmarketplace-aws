a2c8d3be639f30c05448f033c51da1df9b78f0f2
benvand <benjaminfvandersteen@gmail.com>
2019-03-27 15:51:04 +0000

    Workaround to make arns appear as ids

--- terraform/modules/antivirus-sns/s3-notifications.tf
+++ terraform/modules/antivirus-sns/s3-notifications.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  count  = "${length(var.bucket_ids)}"
-  bucket = "${var.bucket_ids[count.index]}"
+  count = "${length(var.bucket_arns)}"
+
+  // Bucket identifiers are passed as arns but we require just 'id'/name here
+  bucket = "${replace(var.bucket_arns[count.index], "arn:aws:s3:::", "")}"
 
   topic {
     topic_arn = "${aws_sns_topic.s3_file_upload_notification.arn}"
