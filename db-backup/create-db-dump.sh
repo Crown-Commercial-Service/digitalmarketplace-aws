90468b518612c106ecff7d9aed2115a02d9a4543
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-05-16 10:44:27 +0100

    Don't log DB service credentials to Jenkins

--- db-backup/create-db-dump.sh
+++ db-backup/create-db-dump.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+set +x  # Don't log credentials
 DB_URI=$(echo $VCAP_SERVICES | jq -r '.postgres[0].credentials.uri')
+set -x  # Restore logging
 echo -n "${PUBKEY}" > /app/public.key
 gpg2 --import /app/public.key
 pg_dump "${DB_URI}" --no-acl --no-owner --clean --if-exists | gzip | \
