a571bbf5ea8a6711338a12ffcf2fdaaf3d916f1a
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-05-23 11:20:13 +0100

    main jenkins and cloudtrail buckets

--- terraform/modules/jenkins/log_bucket/s3_bucket.tf
+++ terraform/modules/jenkins/log_bucket/s3_bucket.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "jenkins_logs_bucket" {
   bucket        = "${var.name}"
   force_destroy = true
+  versioning {
+    enabled = true
+  }
 
   # The id 156460612806 is the ELB account (eu-west-1)
   # see https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-access-logs.html
@@ -9,6 +12,17 @@ resource "aws_s3_bucket" "jenkins_logs_bucket" {
 {
     "Version": "2012-10-17",
     "Statement": [
+        {
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "*",
+            "Resource": "arn:aws:s3:::${var.name}/*",
+            "Condition": {
+            "Bool": {
+                "aws:SecureTransport": "false"
+            }
+            }
+        },
         {
             "Sid": "AWSElasticLoadBalancerWrite",
             "Effect": "Allow",
