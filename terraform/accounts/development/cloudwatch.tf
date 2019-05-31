fbc44cc2817bc60190adda76f0f5e0ee297b5b0e
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-05-31 12:50:12 +0100

    Fix typo in bucket name

--- terraform/accounts/development/cloudwatch.tf
+++ terraform/accounts/development/cloudwatch.tf
@@ -1,6 +1,6 @@
 module "cloudtrail" {
   source              = "../../modules/cloudtrail/cloudtrail"
   aws_main_account_id = "${var.aws_main_account_id}"
-  s3_bucket_name      = "digitalmarketplace-dev-account-cloudtrail-bucket"
+  s3_bucket_name      = "digitalmarketplaces-dev-account-cloudtrail-bucket"
   trail_name          = "digitalmarketplaces-dev-account-cloudtrail"
 }
