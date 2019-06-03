06cac97c6331cae679b5bc70872f49f625a1761a
benvand <benjaminfvandersteen@gmail.com>
2019-06-03 09:23:19 +0100

    Add APPLICATION_NAME arg to ip-whitelist script so script only adds route relevant to current deployment

--- scripts/add-ip-whitelist-route-service.sh
+++ scripts/add-ip-whitelist-route-service.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "This script uses the variables in vars/ip_whitelist_routes.json to ip whitelist routes using the re-ip-whitelist-service."
     echo ""
-    echo "Usage: ./scripts/add-ip-whitelist-route-service.sh <stage>"
+    echo "Usage: ./scripts/add-ip-whitelist-route-service.sh <STAGE> <APPLICATION_NAME>"
     exit 1
 fi
 
 export STAGE=$( echo $1 | awk '{ print tolower($0) }' )
+export APPLICATION_NAME=$( echo $2 | awk '{ print tolower($0) }' )
 
 IFS=$'\n'
-for HOSTNAME_PATH in $(jq -r '.[env.STAGE][] | "\(.hostname) \(.path)"' vars/ip_whitelist_routes.json); do
+for HOSTNAME_PATH in $(jq -r '.[env.STAGE][env.APPLICATION_NAME][] | "\(.hostname) \(.path)"' vars/ip_whitelist_routes.json); do
   HOSTNAME=$(echo $HOSTNAME_PATH | cut -f1 -d" ")
   ROUTE_PATH=$(echo $HOSTNAME_PATH | cut -f2 -d" ")
   echo cf bind-route-service cloudapps.digital re-ip-whitelist-service --hostname $HOSTNAME --path $ROUTE_PATH
