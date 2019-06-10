0bbe779379b791d5a69e79caba0df581269f48a8
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-06-10 12:35:08 +0100

    digitalmarketplace not digitalmarketplaces

--- terraform/accounts/backups/cloudwatch.tf
+++ terraform/accounts/backups/cloudwatch.tf
@@ -1,7 +1,7 @@
 module "cloudtrail" {
   source              = "../../modules/cloudtrail"
   account_id          = "${var.aws_backups_account_id}"
-  s3_bucket_name      = "digitalmarketplaces-backups-account-cloudtrail-bucket"
+  s3_bucket_name      = "digitalmarketplace-backups-account-cloudtrail-bucket"
   trail_name          = "digitalmarketplace-backups-account-cloudtrail"
   validate_account_id = "${var.aws_main_account_id}"
 }
