8d937694cfa92d30ee36bb08ff3454d836aa3f6d
benvand <benjaminfvandersteen@gmail.com>
2019-07-10 09:23:19 +0100

    Move ssh keypair creation out of jenkins module
    * Extrapolate out the creation of this keypair.
      * A key does not necessarily need to  be tied to a single instance.
      * Makes creating new Jenki simpler

--- terraform/accounts/main/main.tf
+++ terraform/accounts/main/main.tf
@@ -58,12 +58,16 @@ module "jenkins_elb_log_bucket" {
   name   = "jenkins-ci.marketplace.team-logs-bucket"
 }
 
+resource "aws_key_pair" "jenkins" {
+  key_name   = "${var.ssh_key_name}"
+  public_key = "${var.jenkins_public_key}" # injected by Makefile-common
+}
+
 module "jenkins" {
   source                        = "../../modules/jenkins/jenkins"
   name                          = "jenkins"
   dev_user_ips                  = "${var.dev_user_ips}"
-  jenkins_public_key_name       = "${var.ssh_key_name}"                                         # confusingly-named variable in terraform/common.json
-  jenkins_public_key            = "${var.jenkins_public_key}"
+  jenkins_public_key_name       = "${aws_key_pair.jenkins.key_name}"
   jenkins_instance_profile      = "${aws_iam_instance_profile.jenkins.name}"
   jenkins_wildcard_elb_cert_arn = "${aws_acm_certificate.jenkins_wildcard_elb_certificate.arn}"
   ami_id                        = "ami-2a7d75c0"
