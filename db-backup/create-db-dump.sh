bd3f31898a155c5832374da9262f719ee1de1887
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-06-23 14:04:12 +0100

    use built in PaaS env var

--- db-backup/create-db-dump.sh
+++ db-backup/create-db-dump.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-set +x  # Don't log credentials
-DB_URI=$(echo $VCAP_SERVICES | jq -r '.postgres[0].credentials.uri')
-set -x  # Restore logging
 echo -n "${PUBKEY}" > /app/public.key
 gpg2 --import /app/public.key
-pg_dump "${DB_URI}" --no-acl --no-owner --clean --if-exists | gzip | \
+pg_dump "${DATABASE_URL}" --no-acl --no-owner --clean --if-exists | gzip | \
   gpg2 --trust-model always -r "${RECIPIENT}" --out /app/"${DUMP_FILE_NAME}" --encrypt
 
 /usr/local/bin/python /app/upload-dump-to-s3.py
