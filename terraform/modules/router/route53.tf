c497987cf403eb47f561c296b3f279bf04a7db47
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-10-19 16:40:50 +0100

    upgrade router

--- terraform/modules/router/route53.tf
+++ terraform/modules/router/route53.tf
@@ -1,118 +1,124 @@
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.70"
+}
+
 resource "aws_route53_zone" "root" {
-  name = "${var.domain}"
+  name = var.domain
 }
 
 resource "aws_route53_record" "www_acm_validation" {
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  zone_id = aws_route53_zone.root.zone_id
   name    = "${var.www_acm_name}${var.domain}"
   type    = "CNAME"
   ttl     = "86400"
 
   records = [
-    "${var.www_acm_value}",
+    var.www_acm_value,
   ]
 }
 
 resource "aws_route53_record" "api_acm_validation" {
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  zone_id = aws_route53_zone.root.zone_id
   name    = "${var.api_acm_name}${var.domain}"
   type    = "CNAME"
   ttl     = "86400"
 
   records = [
-    "${var.api_acm_value}",
+    var.api_acm_value,
   ]
 }
 
 resource "aws_route53_record" "search_api_acm_validation" {
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  zone_id = aws_route53_zone.root.zone_id
   name    = "${var.search_api_acm_name}${var.domain}"
   type    = "CNAME"
   ttl     = "86400"
 
   records = [
-    "${var.search_api_acm_value}",
+    var.search_api_acm_value,
   ]
 }
 
 resource "aws_route53_record" "antivirus_api_acm_validation" {
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  zone_id = aws_route53_zone.root.zone_id
   name    = "${var.antivirus_api_acm_name}${var.domain}"
   type    = "CNAME"
   ttl     = "86400"
 
   records = [
-    "${var.antivirus_api_acm_value}",
+    var.antivirus_api_acm_value,
   ]
 }
 
 resource "aws_route53_record" "assets_acm_validation" {
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  zone_id = aws_route53_zone.root.zone_id
   name    = "${var.www_acm_name}${var.domain}"
   type    = "CNAME"
   ttl     = "86400"
 
   records = [
-    "${var.assets_acm_value}",
+    var.assets_acm_value,
   ]
 }
 
 resource "aws_route53_record" "www" {
-  count   = "${length(var.cname_domain) == 0 ? 0 : 1}"
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  count   = length(var.cname_domain) == 0 ? 0 : 1
+  zone_id = aws_route53_zone.root.zone_id
   name    = "www.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
 
   records = [
-    "${var.cname_domain}",
+    var.cname_domain,
   ]
 }
 
 resource "aws_route53_record" "api" {
-  count   = "${length(var.cname_domain) == 0 ? 0 : 1}"
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  count   = length(var.cname_domain) == 0 ? 0 : 1
+  zone_id = aws_route53_zone.root.zone_id
   name    = "api.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
 
   records = [
-    "${var.cname_domain}",
+    var.cname_domain,
   ]
 }
 
 resource "aws_route53_record" "search_api" {
-  count   = "${length(var.cname_domain) == 0 ? 0 : 1}"
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  count   = length(var.cname_domain) == 0 ? 0 : 1
+  zone_id = aws_route53_zone.root.zone_id
   name    = "search-api.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
 
   records = [
-    "${var.cname_domain}",
+    var.cname_domain,
   ]
 }
 
 resource "aws_route53_record" "antivirus_api" {
-  count   = "${length(var.cname_domain) == 0 ? 0 : 1}"
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  count   = length(var.cname_domain) == 0 ? 0 : 1
+  zone_id = aws_route53_zone.root.zone_id
   name    = "antivirus-api.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
 
   records = [
-    "${var.cname_domain}",
+    var.cname_domain,
   ]
 }
 
 resource "aws_route53_record" "assets" {
-  count   = "${length(var.cname_domain) == 0 ? 0 : 1}"
-  zone_id = "${aws_route53_zone.root.zone_id}"
+  count   = length(var.cname_domain) == 0 ? 0 : 1
+  zone_id = aws_route53_zone.root.zone_id
   name    = "assets.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
 
   records = [
-    "${var.cname_domain}",
+    var.cname_domain,
   ]
 }
+
