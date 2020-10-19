b57ea7b787fc163b0e0482646097fb65e7b1f3bb
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-10-19 16:16:20 +0100

    upgrade cloudtrail

--- terraform/modules/cloudtrail/main.tf
+++ terraform/modules/cloudtrail/main.tf
@@ -1,25 +1,31 @@
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.70"
+}
+
 module "cloudtrail-bucket" {
   source         = "./modules/cloudtrail-bucket"
-  s3_bucket_name = "${var.s3_bucket_name}"
+  s3_bucket_name = var.s3_bucket_name
 }
 
 module "cloudtrail-cloudwatch" {
   source            = "./modules/cloudtrail-cloudwatch"
-  trail_name        = "${var.trail_name}"
-  retention_in_days = 731                               // As per RE convention on keeping logs for 2 years
+  trail_name        = var.trail_name
+  retention_in_days = 731 // As per RE convention on keeping logs for 2 years
 }
 
 module "cloudtrail-validate-logs-role" {
   source          = "./modules/cloudtrail-validate-logs-role"
   assume_role_arn = "arn:aws:iam::${var.validate_account_id}:root"
-  s3_bucket_arn   = "${module.cloudtrail-bucket.s3_bucket_arn}"
+  s3_bucket_arn   = module.cloudtrail-bucket.s3_bucket_arn
 }
 
 module "cloudtrail-cloudtrail" {
   source                     = "./modules/cloudtrail-cloudtrail"
-  trail_name                 = "${var.trail_name}"
-  s3_bucket_name             = "${var.s3_bucket_name}"
-  s3_bucket_key_prefix       = "${var.account_id}"
-  cloud_watch_logs_role_arn  = "${module.cloudtrail-cloudwatch.cloud_watch_logs_role_arn}"
-  cloud_watch_logs_group_arn = "${module.cloudtrail-cloudwatch.cloud_watch_logs_group_arn}"
+  trail_name                 = var.trail_name
+  s3_bucket_name             = var.s3_bucket_name
+  s3_bucket_key_prefix       = var.account_id
+  cloud_watch_logs_role_arn  = module.cloudtrail-cloudwatch.cloud_watch_logs_role_arn
+  cloud_watch_logs_group_arn = module.cloudtrail-cloudwatch.cloud_watch_logs_group_arn
 }
+
--- terraform/modules/cloudtrail/variables.tf
+++ terraform/modules/cloudtrail/variables.tf
@@ -2,10 +2,13 @@ variable "account_id" {
   description = "The ID of the account, used as the S3 bucket key prefix"
 }
 
-variable "s3_bucket_name" {}
+variable "s3_bucket_name" {
+}
 
-variable "trail_name" {}
+variable "trail_name" {
+}
 
 variable "validate_account_id" {
   description = "The ID of the account that can assume the role for validating logs"
 }
+
