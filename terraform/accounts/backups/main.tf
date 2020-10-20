c2b7c5af9d0e053eab9ef106610459d5c47b98f6
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-10-20 13:20:54 +0100

    update CSW role

--- terraform/accounts/backups/main.tf
+++ terraform/accounts/backups/main.tf
@@ -1,27 +1,18 @@
 provider "aws" {
   region  = "eu-west-1"
-  version = "1.9.0"
 }
 
 provider "aws" {
   alias   = "london"
   region  = "eu-west-2"
-  version = "1.9.0"
 }
 
 resource "aws_iam_account_alias" "alias" {
   account_alias = "digitalmarketplace-backups"
 }
 
-# TODO remove old csw_inspector_role in favour of new gds_security_audit_role when switch to new role  is completed by secops
-
-module "csw_inspector_role" {
-  source               = "git::https://github.com/alphagov/csw-client-role.git?ref=v1.2"
-  csw_agent_account_id = "${var.csw_agent_account_id}"
-}
-
-module "gds_security_audit_role" {
-  source           = "git::https://github.com/alphagov/tech-ops.git?ref=13f54e5//cyber-security/modules/gds_security_audit_role"
+module "cyber_security_audit_role" {
+  source = "git::https://github.com/alphagov/tech-ops//cyber-security/modules/gds_security_audit_role?ref=720885a9769c40942ff30b32179e1fad18f2ca10"
   chain_account_id = "${var.gds_security_audit_chain_account_id}"
 }
 
