ab5819fc713eda800fd99df3b8c61598d2d271d0
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2020-11-27 12:36:25 +0000

    Add `ReadOnlyAccess` built in policy[1] to `security_audit` group.
    [1] https://console.aws.amazon.com/iam/home?region=eu-west-1#/policies/arn:aws:iam::aws:policy/ReadOnlyAccess$serviceLevelSummary

--- terraform/modules/iam-users/security_audit.tf
+++ terraform/modules/iam-users/security_audit.tf
@@ -7,6 +7,11 @@ resource "aws_iam_group_policy_attachment" "security_audit_policy" {
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
 
+resource "aws_iam_group_policy_attachment" "read_only_access_policy" {
+  group      = "${aws_iam_group.security_audit.name}"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 resource "aws_iam_group_policy_attachment" "developers_ip_restriced" {
   group      = "${aws_iam_group.security_audit.name}"
   policy_arn = "${var.ip_restricted_access_policy_arn}"
@@ -17,4 +22,9 @@ resource "aws_iam_group_membership" "security_audit" {
   users      = ["${var.security_audit_users}"]
   group      = "${aws_iam_group.security_audit.name}"
   depends_on = ["module.users"]
+}
+
+resource "aws_iam_group_policy_attachment" "security_audit_iam_manage_account" {
+  group      = "${aws_iam_group.security_audit.name}"
+  policy_arn = "${var.iam_manage_account_policy_arn}"
 }
