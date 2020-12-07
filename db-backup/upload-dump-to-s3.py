e26faf99bdfe3b3b1fed7920893a9b7551348827
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2020-12-07 09:36:43 +0000

    use curl to perform upload

--- db-backup/upload-dump-to-s3.py
+++ db-backup/upload-dump-to-s3.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import json
-import requests
-from requests.exceptions import HTTPError
+import subprocess
 
 
 def upload_dump_to_s3():
     s3_post_url_data = json.loads(os.environ['S3_POST_URL_DATA'])
     dump_file = "/app/{}".format(os.environ['DUMP_FILE_NAME'])
-
     url = s3_post_url_data['url']
     fields = s3_post_url_data['fields']
-    files = {"file": open(dump_file, 'rb')}
-
-    response = requests.post(url, data=fields, files=files)
-
+    curl_args = ['curl', '--fail']
+    for k, v in fields.items():
+        curl_args.append('-F')
+        curl_args.append(f"{k}={v}")
+    curl_args.append('-F')
+    curl_args.append(f"file=@{dump_file}")
+    curl_args.append(url)
     try:
-        response.raise_for_status()
-    except HTTPError as e:
-        print("Error uploading {} to {}: {}".format(dump_file, url, e.args[0]))
-        sys.exit(1)
-    except Exception as e:
-        print("Error uploading: {}".format(e))
-        sys.exit(2)
-    else:
-        print('Successfully uploaded {} to {}'.format(dump_file, url))
+        response = subprocess.check_output(curl_args)
+        print(response)
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Upload failed with error: " + e.output)
+
+    print('Successfully uploaded {} to {}'.format(dump_file, url))
 
 
 if __name__ == "__main__":
