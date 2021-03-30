bdd28444e32afbd4abd5e7991880939fbedd4f6c
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2021-03-30 13:56:29 +0100

    Refactor Jenkins Route53 DNS validation
    In v3 and up of the terraform AWS provider, the `domain_validation_options`
    property of an `aws_acm_certificate` is now an optional list of
    values to allow for manual email verification. This means we can
    no longer access values by index position.
    
    Refactor to instead use `for_each` to iterate through all potential
    values in the list.

--- terraform/accounts/main/route53.tf
+++ terraform/accounts/main/route53.tf
@@ -46,15 +46,22 @@ resource "aws_acm_certificate" "jenkins_wildcard_elb_certificate" {
 }
 
 resource "aws_route53_record" "jenkins_wildcard_elb_cert_validation" {
-  name    = aws_acm_certificate.jenkins_wildcard_elb_certificate.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.jenkins_wildcard_elb_certificate.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.jenkins_wildcard_elb_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
   zone_id = aws_route53_zone.marketplace_team.id
-  records = [aws_acm_certificate.jenkins_wildcard_elb_certificate.domain_validation_options[0].resource_record_value]
+  records = [each.value.record]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "jenkins_wildcard_elb_certificate" {
   certificate_arn         = aws_acm_certificate.jenkins_wildcard_elb_certificate.arn
-  validation_record_fqdns = [aws_route53_record.jenkins_wildcard_elb_cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.jenkins_wildcard_elb_cert_validation : record.fqdn]
 }
-
