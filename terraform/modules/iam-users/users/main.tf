f5a1c366fdf7789ee0ee6112af8c96ee2c76e437
George <george.hill@crowncommercial.gov.uk>
2021-09-08 13:00:19 +0100

    Adding CCS TechOps Engineers

--- terraform/modules/iam-users/users/main.tf
+++ terraform/modules/iam-users/users/main.tf
@@ -32,3 +32,23 @@ resource "aws_iam_user" "jamieeunson" {
   name          = "jamieeunson"
   force_destroy = true
 }
+
+resource "aws_iam_user" "jackjones" {
+  name          = "jackjones"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "willtrust" {
+  name          = "willtrust"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "johndenford" {
+  name          = "johndenford"
+  force_destroy = true
+}
+
+resource "aws_iam_user" "crispinosborne" {
+  name          = "crispinosborne"
+  force_destroy = true
+}
