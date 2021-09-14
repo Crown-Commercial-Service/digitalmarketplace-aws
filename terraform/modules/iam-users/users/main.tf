b39f13c85afc9c5fc03906f06fc1a689427dd371
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-09-14 16:39:10 +0100

    Remove Alex and Brett
    They've both just left the team: https://crown-commercial-service.github.io/digitalmarketplace-manual/managing-people-and-secret-things/accounts.html#aws

--- terraform/modules/iam-users/users/main.tf
+++ terraform/modules/iam-users/users/main.tf
@@ -8,18 +8,8 @@ resource "aws_iam_user" "paula_valenca" {
   force_destroy = true
 }
 
-resource "aws_iam_user" "brett_kyle" {
-  name          = "brettkyle"
-  force_destroy = true
-}
-
 resource "aws_iam_user" "benjamin_gill" {
   name          = "benjamingill"
-  force_destroy = true
-}
-
-resource "aws_iam_user" "alex_smith" {
-  name          = "alexsmith"
   force_destroy = true
 }
 
