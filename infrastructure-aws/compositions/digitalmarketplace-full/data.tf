29133c3c7aeb2fff91d92e1bb9f31619409e5ba5
George <george.hill@crowncommercial.gov.uk>
2023-02-14 11:36:03 +0000

    Add IAM Policy Document for Jenkins Cross Account access

--- infrastructure-aws/compositions/digitalmarketplace-full/data.tf
+++ infrastructure-aws/compositions/digitalmarketplace-full/data.tf
@@ -12,3 +12,16 @@ data "aws_iam_policy_document" "apprunner_service_deployment_policy" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "apprunner_deployment_jenkins_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["arn:aws:iam::${var.jenkins_account_id}:root"]
+      type        = "AWS"
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
