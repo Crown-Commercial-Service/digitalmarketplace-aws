32e72b728071b14429e8ce489aa3fd03f8f908fd
Mark Henwood <mcbhenwood@icloud.com>
2023-02-20 16:05:16 +0000

    Move ALB to public subnet; restrict security groups
    Remove reliance upon default VPC SG

--- infrastructure-aws/compositions/digitalmarketplace-full/redis_cache.tf
+++ infrastructure-aws/compositions/digitalmarketplace-full/redis_cache.tf
@@ -6,11 +6,33 @@ resource "aws_elasticache_cluster" "frontend_sessions" {
   parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
-  security_group_ids   = [module.dmp_vpc.default_security_group_id]
+  security_group_ids   = [aws_security_group.frontend_session_cache.id]
   subnet_group_name    = aws_elasticache_subnet_group.frontend_sessions.name
 }
 
 resource "aws_elasticache_subnet_group" "frontend_sessions" {
   name       = "${var.project_name}-${var.environment_name}-session-cache"
   subnet_ids = module.dmp_vpc.private_subnet_ids
+}
+
+
+resource "aws_security_group" "frontend_session_cache" {
+  name        = "${var.environment_name}-session-cache"
+  description = "The ${var.environment_name} Redis cache"
+  vpc_id      = module.dmp_vpc.vpc_id
+
+  tags = {
+    Name = "${var.environment_name}-session-cache"
+  }
+}
+
+resource "aws_security_group_rule" "redis_from_services" {
+  description = "Allow Redis traffic from the services"
+
+  security_group_id        = aws_security_group.frontend_session_cache.id
+  from_port                = 6379
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.alb_target_group.id
+  to_port                  = 6379
+  type                     = "ingress"
 }
--- infrastructure-aws/environments/staging/main.tf
+++ infrastructure-aws/environments/staging/main.tf
@@ -3,7 +3,9 @@ module "digitalmarketplace_full" {
 
   aws_region                      = var.aws_region
   aws_target_account              = data.aws_caller_identity.current.account_id
+  domain_name                     = var.domain_name
   environment_name                = var.environment_name
+  hosted_zone_id                  = var.hosted_zone_id
   project_name                    = var.project_name
   services_desired_counts         = var.services_desired_counts
   vpc_cidr_block                  = var.vpc_cidr_block
