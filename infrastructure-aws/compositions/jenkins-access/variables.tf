09d69524f2289e2d748361edf75528a79ba01dcf
George <george.hill@crowncommercial.gov.uk>
2023-02-21 15:20:16 +0000

    Create Jenkins Access composition to allow ECS deployments - Create an IAM Role to be leveraged by Jenkins to deploy to ECS - Capture all relevant policies and attachments as necessary - Create composition for Jenkins access - Add new Jenkins composition to environments/staging - Add variables for aws_target_account, jenkins_account_id, and Terraform state resources

--- infrastructure-aws/compositions/jenkins-access/variables.tf
+++ infrastructure-aws/compositions/jenkins-access/variables.tf
@@ -3,19 +3,14 @@ variable "aws_region" {
   description = "Region for resource deployment"
 }
 
-variable "domain_name" {
+variable "aws_target_account" {
   type        = string
-  description = "Domain name to use in public-facing ingress cert and URL"
+  description = "ID of the account into which deployments are performed"
 }
 
 variable "environment_name" {
   type        = string
   description = "Name to indicate purpose of environment"
-}
-
-variable "hosted_zone_id" {
-  type        = string
-  description = "ID of the Route 53 Hosted Zone which will manage the DNS and cert validation for this environment"
 }
 
 variable "jenkins_account_id" {
@@ -28,11 +23,6 @@ variable "project_name" {
   description = "Namespace to prepend to resource names where hierarchy is required"
 }
 
-variable "services_desired_counts" {
-  type        = map(number)
-  description = "Desired number of instances for each service"
-}
-
 variable "terraform_state_s3_bucket_name" {
   type        = string
   description = "The name of the S3 bucket holding the Terraform State file"
@@ -41,19 +31,4 @@ variable "terraform_state_s3_bucket_name" {
 variable "terraform_state_dynamodb_table_name" {
   type        = string
   description = "The name of the DynamoDB table holding the Lock files for the Terraform State"
-}
-
-variable "vpc_cidr_block" {
-  type        = string
-  description = "CIDR block for VPC"
-}
-
-variable "vpc_private_subnets_cidr_blocks" {
-  type        = map(string)
-  description = "CIDR blocks for private subnets"
-}
-
-variable "vpc_public_subnets_cidr_blocks" {
-  type        = map(string)
-  description = "CIDR blocks for public subnets"
 }
--- infrastructure-aws/environments/staging/main.tf
+++ infrastructure-aws/environments/staging/main.tf
@@ -1,14 +1,25 @@
 module "digitalmarketplace_full" {
   source = "../../compositions/digitalmarketplace-full"
 
-  aws_region                      = var.aws_region
-  aws_target_account              = data.aws_caller_identity.current.account_id
-  domain_name                     = var.domain_name
-  environment_name                = var.environment_name
-  hosted_zone_id                  = var.hosted_zone_id
-  project_name                    = var.project_name
-  services_desired_counts         = var.services_desired_counts
-  vpc_cidr_block                  = var.vpc_cidr_block
-  vpc_private_subnets_cidr_blocks = var.vpc_private_subnets_cidr_blocks
-  vpc_public_subnets_cidr_blocks  = var.vpc_public_subnets_cidr_blocks
+  aws_region                          = var.aws_region
+  aws_target_account                  = data.aws_caller_identity.current.account_id
+  domain_name                         = var.domain_name
+  environment_name                    = var.environment_name
+  hosted_zone_id                      = var.hosted_zone_id
+  project_name                        = var.project_name
+  services_desired_counts             = var.services_desired_counts
+  vpc_cidr_block                      = var.vpc_cidr_block
+  vpc_private_subnets_cidr_blocks     = var.vpc_private_subnets_cidr_blocks
+  vpc_public_subnets_cidr_blocks      = var.vpc_public_subnets_cidr_blocks
+}
+
+module "jenkins_access" {
+  source                              = "../../compositions/jenkins-access"
+  aws_region                          = var.aws_region
+  aws_target_account                  = data.aws_caller_identity.current.account_id
+  environment_name                    = var.environment_name
+  jenkins_account_id                  = var.jenkins_account_id
+  project_name                        = var.project_name
+  terraform_state_dynamodb_table_name = var.terraform_state_dynamodb_table_name
+  terraform_state_s3_bucket_name      = var.terraform_state_s3_bucket_name
 }
